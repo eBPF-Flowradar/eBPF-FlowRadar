971d71be4ccfea3777c3cf3bdda51af303543db1
Alen Antony <alenantonyme@gmail.com>
2024-03-23 04:30:48 +0000

    just

--- counter_decode.c
+++ counter_decode.c
@@ -4,5 +4,9 @@
 int CounterDecode(struct flowset en_flowset, int *decode_flows,
                   int decode_flows_len, int *b) {
 
-  int a[en_flowset.][decode_flows_len] = {0};
+  int a[COUNTINGTABLE_SIZE][decode_flows_len] = {0};
+
+  for (int i = 0; i < decode_flows_len; i++) {
+    int flow
+  }
 }
