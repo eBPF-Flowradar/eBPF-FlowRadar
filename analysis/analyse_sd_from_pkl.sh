6718b1f7a11d659f35aab82f43eadc57168ce6d2
Alen Antony <alenantonyme@gmail.com>
2024-04-24 06:02:22 +0000

    made changes to move the detect logs also

--- analysis/analyse_sd_from_pkl.sh
+++ analysis/analyse_sd_from_pkl.sh
@@ -2,13 +2,17 @@
 
 set -e
 
-LOGS_PATH="/home/anonymous/eBPF-FlowRadar/sd_logs.csv"
+LOGS_PATH="/home/anonymous/eBPF-FlowRadar"
+SD_LOGS="sd_logs.csv"
+DETECT_LOGS="detect.csv"
 ANALYSIS_FILE="analysis_sd_pkl.txt"
 RES_DIR="results/$(date +"%Y-%m-%d:%H-%M-%S")_sd_pkl"
 PY_SCRIPT="analysis/analyse_sd_from_pkl.py"
 
-scp anonymous@192.168.122.246:$LOGS_PATH ./
-ssh anonymous@192.168.122.246 "rm $LOGS_PATH"
+scp anonymous@192.168.122.246:$LOGS_PATH/$SD_LOGS ./
+ssh anonymous@192.168.122.246 "rm $LOGS_PATH/$SD_LOGS"
+scp anonymous@192.168.122.246:$LOGS_PATH/$DETECT_LOGS ./
+ssh anonymous@192.168.122.246 "rm $LOGS_PATH/$DETECT_LOGS"
 python3 $PY_SCRIPT > $ANALYSIS_FILE
 cat $ANALYSIS_FILE
 
@@ -21,7 +25,7 @@ fi
 mkdir "$RES_DIR"
 
 # Move the files to the new directory
-mv "sd_logs.csv" "$ANALYSIS_FILE" "./$RES_DIR/"
+mv "$SD_LOGS" "$DETECT_LOGS" "$ANALYSIS_FILE" "./$RES_DIR/"
 
 echo -e "\nFiles moved to directory: $RES_DIR"
 
