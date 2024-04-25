6f945ee41391f214c26c4d5fa9800d86fd4085d6
Anonymous-AAA <alenantonyme@gmail.com>
2024-04-25 09:30:58 +0530

    updated script for analysing a single folder

--- analysis/run.sh
+++ analysis/run.sh
@@ -4,10 +4,11 @@
 REMOTE_HOME_FOLDER="/home/anonymous/eBPF-FlowRadar"
 REM_INTERFACE="enp7s0"
 HOST_INTERFACE="veth1"
-#PCAP_FILE="analysis/pcap_files/output_with_eth.pcap"
 SLEEP_SEC=10
 DECODABLE_CSV_FILE="decodable.csv"
-PCAP_FOLDER="/home/anonymousa/Projects/eBPF-FlowRadar/analysis/pcap_files/attack_files/cia"
+PARENT_FOLDER="/home/anonymousa/Projects/eBPF-FlowRadar/analysis/pcap_files"
+FINAL_RESULTS_FOLDER="/home/anonymousa/Projects/eBPF-FlowRadar/final_results"
+RESULTS_FOLDER="/home/anonymousa/Projects/eBPF-FlowRadar/results"
 
 #delete before analysis
 sudo rm $DECODABLE_CSV_FILE
@@ -15,45 +16,56 @@ sudo rm $DECODABLE_CSV_FILE
 #exit on error
 set -e
 
-# Loop through each file in the folder
-for file in "$PCAP_FOLDER"/*; do
-    # Check if the file is a regular file (not a directory)
-    if [ -f "$file" ]; then
+find "$PARENT_FOLDER" -maxdepth 1 -mindepth 1 -type d -print0 |
+while IFS= read -r -d '' PCAP_FOLDER; do
+    echo "Processing folder: $folder"
 
-        echo "Processing file: $file"
+    # Loop through each file in the folder
+    for file in "$PCAP_FOLDER"/*; do
+	# Check if the file is a regular file (not a directory)
+	if [ -f "$file" ]; then
 
-	#start flowradar
-	echo "Start flowradar"
-	ssh root@192.168.122.246 "cd $REMOTE_HOME_FOLDER;nohup ./flowradar $REM_INTERFACE > /dev/null 2>&1 </dev/null &" 
+	    echo "Processing file: $file"
 
-	sleep 3
+	    #start flowradar
+	    echo "Start flowradar"
+	    ssh root@192.168.122.246 "cd $REMOTE_HOME_FOLDER;nohup ./flowradar $REM_INTERFACE > /dev/null 2>&1 </dev/null &"  </dev/null
 
-	#start tcp replay
-	echo "Start tcpreplay"
-	sudo tcpreplay -i $HOST_INTERFACE $file 
+	    sleep 3
 
-
-	#sleep
-	echo "Sleep for $SLEEP_SEC seconds "
-	sleep $SLEEP_SEC
-
-
-	#Stop flowradar
-	echo "Stopping flowradar"
-	ssh root@192.168.122.246 "pkill -f flowradar" #remote
-
-	
+	    #start tcp replay
+	    echo "Start tcpreplay"
+	    sudo tcpreplay -i $HOST_INTERFACE $file 
 
 
-	#Do analysis
-	echo "Start analysis"
-	./analysis/analyse_sd_from_pkl.sh $(basename "$file")
+	    #sleep
+	    echo "Sleep for $SLEEP_SEC seconds "
+	    sleep $SLEEP_SEC
 
-	echo "$file Done"
 
-    fi
+	    #Stop flowradar
+	    echo "Stopping flowradar"
+	    ssh root@192.168.122.246 "pkill -f flowradar" </dev/null  #remote
+
+	    
+
+
+	    #Do analysis
+	    echo "Start analysis"
+	    ./analysis/analyse_sd_from_pkl.sh $(basename "$file") </dev/null
+
+	    echo "$file Done"
+
+	fi
+    done
+    
+    folder_name=$(basename "$PCAP_FOLDER") 
+    mkdir $FINAL_RESULTS_FOLDER/$folder_name
+    mv $RESULTS_FOLDER/*  $FINAL_RESULTS_FOLDER/$folder_name
+    mv $DECODABLE_CSV_FILE $FINAL_RESULTS_FOLDER/$folder_name
+    echo
+    echo "Completed analysis of $folder_name"
+    echo
 done
 
-echo "Completed analysis"
-
-
+echo "All analysis done"
