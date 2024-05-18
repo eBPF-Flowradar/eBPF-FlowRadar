f61893ea704910b4f4ce7d3c5201e883c78063ab
Alen Antony <alenantonyme@gmail.com>
2024-05-18 05:26:59 +0000

    improved scale

--- analysis/send_pkts.sh
+++ analysis/send_pkts.sh
@@ -21,20 +21,7 @@ fi
 # Count the total number of packets in the pcap file
 PACKET_COUNT=$(capinfos "$PCAP_FILE" | grep "Number of packets ="| tr -d " " | cut -d "=" -f 2)
 
-# Calculate packets per second (PPS)
-# if [ "$DURATION" -eq 0 ]; then
-#     echo "Error: duration must be greater than 0"
-#     exit 1
-# fi
-
-PPS=$(echo "scale=2; $PACKET_COUNT / $DURATION" | bc)
-# PPS=$((PACKET_COUNT / DURATION))
-
-# Verify that PPS is greater than 0
-# if [ "$PPS" -le 0 ]; then
-#     echo "Error: calculated PPS is less than or equal to 0"
-#     exit 1
-# fi
+PPS=$(echo "scale=5; $PACKET_COUNT / $DURATION" | bc)
 
 # Use tcpreplay to send packets at the calculated PPS rate
 sudo tcpreplay --pps="$PPS" -i br0 "$PCAP_FILE"
